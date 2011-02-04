e8547b17bff0b1c8fbb2a694a974e0a796a0075f
kaiserFranz <awildbol@hsz-t.ch>
2011-02-04 14:27:38 +0100

    test failed

--- code/WebServices/src/pasteten/und/moneten/services/Bilanz.java
+++ code/WebServices/src/pasteten/und/moneten/services/Bilanz.java
@@ -12,7 +12,7 @@ import javax.jws.soap.SOAPBinding.Style;
 
 /**
  * Die Bilanz stellt die einzelnen Konten und
- * die darin enthaltenen Betr&auml;ge zur Verfügung.
+ * die darin enthaltenen Beträge zur Verfügung.
  * 
  * @author siddhartha
  * @version 1.01
